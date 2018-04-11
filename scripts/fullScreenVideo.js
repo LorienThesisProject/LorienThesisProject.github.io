b857902dea5623ddc3d797a3d3a024b869bab1c0
Lorien Howe <lorien.howe@gmail.com>
2018-04-11 06:21:30 +0000

    separate out index js

---
+++ scripts/fullScreenVideo.js
@@ -0,0 +1,40 @@
+/*global $*/
+/* Must be included below jquery */
+$(document).ready(function() {
+    // Constants
+    const secondsToWaitBeforeShowingHearTheirStories = 30;
+    
+    var video = document.getElementById("myVideo");
+    let videoHasBeenPlayed = false;
+
+    function playPause() {
+        var el = document.getElementById("playButton");
+        videoHasBeenPlayed = true;
+        if (videoHasBeenPlayed) {
+            setTimeout(function() {
+                $(".media-overlay-contents").css('visibility', 'visible');
+            }, secondsToWaitBeforeShowingHearTheirStories * 1000);
+        }
+        
+        if (video.paused) {
+            video.play();
+            $(".media-play-overlay").css('visibility', 'hidden');
+        }
+        else {
+            video.pause();
+            $(".media-play-overlay").css('visibility', 'visible');
+        }
+    }
+
+    // Event listeners
+    $('.media-overlay-contents').click(() => {
+        document.querySelector('.navbar').scrollIntoView({
+            behavior: 'smooth',
+            block: "start"
+        });
+    });
+    
+    $('#playButton').click(playPause);
+    
+    $('#myVideo').click(playPause);
+});
